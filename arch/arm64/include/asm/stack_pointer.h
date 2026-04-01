440fc8cd16fc68461052aec984f1e71611d71420
Leju <83921102+itsmeleju@users.noreply.github.com>
2026-04-01 21:01:40 +0530

    for clang15

--- arch/arm64/include/asm/stack_pointer.h
+++ arch/arm64/include/asm/stack_pointer.h
@@ -5,6 +5,11 @@
 /*
  * how to get the current stack pointer from C
  */
-register unsigned long current_stack_pointer asm ("sp");
+static __always_inline unsigned long current_stack_pointer(void)
+{
+    unsigned long sp;
+    asm volatile ("mov %0, sp" : "=r" (sp));
+    return sp;
+}
 
 #endif /* __ASM_STACK_POINTER_H */
